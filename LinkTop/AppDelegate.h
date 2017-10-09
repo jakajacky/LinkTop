cdcc8ea4197589935bedf41e64457f825e97fca8
jakajacky <zhangxiaoqiangjoe@gmail.com>
2017-10-09 17:43:17 +0800

    Initial Commit

---
+++ LinkTop/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LinkTop
+//
+//  Created by XiaoQiang on 2017/10/9.
+//  Copyright © 2017年 XiaoQiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
