12acb6558853bc424216d9f1bb8187dbfb7d1289
jakajacky <zhangxiaoqiangjoe@gmail.com>
2017-10-18 14:27:57 +0800

    测量页面UI细节补充完整

--- LinkTop/Measure/Views/MeasureView.m
+++ LinkTop/Measure/Views/MeasureView.m
@@ -25,6 +25,20 @@
 @property (weak, nonatomic) IBOutlet UIView *SPOView;
 @property (weak, nonatomic) IBOutlet UIView *HRView;
 
+// 下面是背景视图
+@property (weak, nonatomic) IBOutlet UIView *rothman_bg;
+@property (weak, nonatomic) IBOutlet UIImageView *rothmanIcon_bg;
+
+@property (weak, nonatomic) IBOutlet UIView *ecg_bg;
+@property (weak, nonatomic) IBOutlet UIImageView *ecgIcon_bg;
+
+@property (weak, nonatomic) IBOutlet UIView *bp_bg;
+
+@property (weak, nonatomic) IBOutlet UIView *temp_bg;
+
+@property (weak, nonatomic) IBOutlet UIView *spo_bg;
+
+@property (weak, nonatomic) IBOutlet UIView *hr_bg;
 
 @end
 
@@ -34,8 +48,11 @@
     [super awakeFromNib];
     // 针对5s特殊适配字体
     [self layoutForiPhone5s];
+    
+    [self layoutBackgroundViews];
 }
 
+#pragma mark - 5s重新布局
 - (void)layoutForiPhone5s {
     NSString *st = [DeviceName platformString];
     if ([st isEqualToString:@"iPhone 5S"]||[st isEqualToString:@"iPhone 5C"]||[st isEqualToString:@"iPhone 5"]) {
@@ -47,6 +64,49 @@
         _hr.font = [UIFont fontWithName:@"PingFangSC-Medium" size:14];
         _hrEN.font = [UIFont fontWithName:@"PingFangSC-Medium" size:10];
     }
+}
+
+#pragma mark - 背景子元素布局
+- (void)layoutBackgroundViews {
+    CGFloat radius       = 4;
+    CGFloat borderwidth  = 0.5;
+    UIColor *borderColor = UIColorHex(#DDDDDD);
+    CGSize shadowOffset  = CGSizeMake(0, 1);
+    UIColor *shadowColor = [UIColor colorWithRed:0.6 green:0.6 blue:0.6 alpha:0.3];
+    
+    
+    _rothmanIcon_bg.layer.cornerRadius = radius;
+    _rothman_bg.layer.cornerRadius     = radius;
+    [_rothman_bg setLayerShadow:shadowColor offset:shadowOffset radius:radius];
+    _rothman_bg.layer.borderWidth = borderwidth;
+    _rothman_bg.layer.borderColor = borderColor.CGColor;
+    
+    _ecgIcon_bg.layer.cornerRadius     = radius;
+    _ecg_bg.layer.cornerRadius         = radius;
+    [_ecg_bg setLayerShadow:shadowColor offset:shadowOffset radius:radius];
+    _ecg_bg.layer.borderWidth = borderwidth;
+    _ecg_bg.layer.borderColor = borderColor.CGColor;
+    
+    _bp_bg.layer.cornerRadius         = radius;
+    [_bp_bg setLayerShadow:shadowColor offset:shadowOffset radius:radius];
+    _bp_bg.layer.borderWidth = borderwidth;
+    _bp_bg.layer.borderColor = borderColor.CGColor;
+    
+    _temp_bg.layer.cornerRadius         = radius;
+    [_temp_bg setLayerShadow:shadowColor offset:shadowOffset radius:radius];
+    _temp_bg.layer.borderWidth = borderwidth;
+    _temp_bg.layer.borderColor = borderColor.CGColor;
+    
+    _spo_bg.layer.cornerRadius         = radius;
+    [_spo_bg setLayerShadow:shadowColor offset:shadowOffset radius:radius];
+    _spo_bg.layer.borderWidth = borderwidth;
+    _spo_bg.layer.borderColor = borderColor.CGColor;
+    
+    _hr_bg.layer.cornerRadius         = radius;
+    [_hr_bg setLayerShadow:shadowColor offset:shadowOffset radius:radius];
+    _hr_bg.layer.borderWidth = borderwidth;
+    _hr_bg.layer.borderColor = borderColor.CGColor;
+    
 }
 
 @end
