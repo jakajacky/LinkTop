f1e940bf0a995ad0da27d2dfe4d32683c8cfe5a6
jakajacky <zhangxiaoqiangjoe@gmail.com>
2017-11-07 17:14:26 +0800

    迁移

--- LinkTop/Supports/DCMVVM/DCDatabase/DCDatabase/DCDB.h
+++
@@ -1,9 +0,0 @@
-
-/*
- 需要引用的库：
- |- libsqlite3.0.dylib
- */
-
-#import "DCDatabaseManager.h"
-#import "DCDatabase.h"
-#import "DCDatabaseObject.h"
--- LinkTop/Supports/DCMVVM/DCHttpRequest/DCHttpRequest/NSString+HttpRequestAdditions.h
+++
@@ -1,9 +0,0 @@
-#import <Foundation/Foundation.h>
-
-@interface NSString (HttpRequestAdditions)
-
-- (NSString*)dchr_urlEncode;
-
-- (NSString *)dchr_urlCombineWithParams:(id)params;
-
-@end
